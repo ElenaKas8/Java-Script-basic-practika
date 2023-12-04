cf3701641e7229cbd8f01000643003ba3a26322a
ElenaK <helenochka4@gmail.com>
2023-12-04 09:16:00 +0100

    updated

--- Lesson_19/script/script1.js
+++ Lesson_19/script/script1.js
@@ -1,4 +1,4 @@
-// Создать форму в html (наименование товара и цена) и добавить скрипт, чтобы при отправке формы данные из нее выводились в консоль.
+//* Создать форму в html (наименование товара и цена) и добавить скрипт, чтобы при отправке формы данные из нее выводились в консоль.
 
 // let form = document.forms.product_form;
 // let productName = document.querySelector('.name_input');
@@ -10,19 +10,19 @@
 //     console.log(productPrice.value);
 // })
 
-// Доработать процесс таким образом, чтобы при отправке формы данные из нее добавлялись в массив с продуктами в виде объекта. Вывести массив в консоль.
+///* Доработать процесс таким образом, чтобы при отправке формы данные из нее добавлялись в массив с продуктами в виде объекта. Вывести массив в консоль.
 
 let form = document.forms.product_form;
-let productName = document.querySelector('.name_input');
-let productPrice = document.querySelector('.price_input');
+let productName = document.querySelector(".name_input");
+let productPrice = document.querySelector(".price_input");
 
 let products = [];
-form.addEventListener('submit', function(event){
-    event.preventDefault();
-    let product_obj = {
-        title: productName.value,
-        price: Number(productPrice.value)
-    }
-    products.push(product_obj);
-    console.log(products);
-})
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+  let product_obj = {
+    title: productName.value,
+    price: Number(productPrice.value),
+  };
+  products.push(product_obj);
+  console.log(products);
+});
